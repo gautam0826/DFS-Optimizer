5ade7abbe784a48af821db12e4640c20d15dcb5e
nkhant <nagie.tk@gmail.com>
2018-03-06 23:35:24 -0800

    Add files via upload

--- gui.py
+++ gui.py
@@ -146,7 +146,8 @@ frame.pack_propagate(False) # Stop frame from resizing to widgets
 top = Frame(frame, width=settings.app.w, height=settings.app.h/2.2, background='white')
 top.grid(row=0)
 top.grid_propagate(False) # Stop frame from resizing to widgets
-top.configure(background = 'snow4')
+top.configure(background = '#212121') # controls back ground for top window---------------------------------------------------------------
+
 for i in range(0,17):
     top.grid_rowconfigure(i, weight=1)
 for j in range(0,49):
@@ -156,7 +157,7 @@ for j in range(0,49):
 # Drop Down List
 #================
 # Display drop down
-displayLabel = Label(top, text='Display Column : ', bg="snow4")
+displayLabel = Label(top, text='Display Column : ', bg="#212121", fg='#FFFFFF')
 displayLabel.grid(row = 12, column = 0, sticky = W)
 displayDropMenu = CreateDropMenu(top, 'Select Status', settings.headerList) #list of headers from imported file
 displayDropMenu.grid(row = 12, column = 1, sticky = W)
@@ -165,12 +166,12 @@ displayDropMenu.grid(row = 12, column = 1, sticky = W)
 
 
 # budget drop down
-budgetLabel = Label(top, text='Budget Column: ', bg="snow4")
+budgetLabel = Label(top, text='Budget Column: ', bg="#212121", fg='#FFFFFF')
 budgetLabel.grid(row = 10, column = 0, sticky = W)
 budgetDropMenu = CreateDropMenu(top, 'Select Status',settings.budgetHeaderList)
 budgetDropMenu.grid(row = 10, column = 1, sticky = W)
 # projections drop down
-projectionsLabel = Label(top, text='Projections Column: ', bg="snow4")
+projectionsLabel = Label(top, text='Projections Column: ', bg="#212121", fg='#FFFFFF')
 projectionsLabel.grid(row = 11, column = 0,sticky = W)
 projectionsDropMenu = CreateDropMenu(top, 'Select Status',settings.projectionsHeaderList)
 projectionsDropMenu.grid(row = 11, column = 1, sticky = W)
@@ -181,25 +182,25 @@ capDropDown.grid(row = 5, column = 4)
 # =============
 # Top Widgets
 # =============
-saveSetting = Button(top, text=' Save Settings ', command=Save, bg="SteelBlue1")
-saveSetting.grid(row=0, column=16, sticky=E)
-loadSetting = Button(top, text=' Load Settings ', command=Load, bg="SteelBlue1")
-loadSetting.grid(row=0, column=17, sticky=W)
+saveSetting = Button(top, text=' Save Settings ', command=Save, highlightbackground="#212121", fg='#FFFFFF')
+saveSetting.grid(row=0, column=16, sticky=S)
+loadSetting = Button(top, text=' Load Settings ', command=Load, highlightbackground="#212121", fg='#FFFFFF')
+loadSetting.grid(row=0, column=17,columnspan=2, sticky=S)
 # addSetting = Button(top, text='Add Setting', command=Add)
 # addSetting.grid(row=0, column=21)
 
-importBtn = Button(top, text='  Import CSV   ', command=Import, bg="SteelBlue1")
-importBtn.grid(row=0, column=20, sticky=E)
-exportBtn = Button(top, text='   Export CSV   ', command=Export, bg="SteelBlue1")
-exportBtn.grid(row=0, column=21, sticky=W)
+importBtn = Button(top, text='  Import CSV   ', command=Import, highlightbackground="#212121", fg='#FFFFFF')
+importBtn.grid(row=0, column=20, sticky=S)
+exportBtn = Button(top, text='   Export CSV   ', command=Export, highlightbackground="#212121", fg='#FFFFFF')
+exportBtn.grid(row=0, column=21, sticky=S)
 
-optimizeBtn = Button(top, text='Optimize', font=('Times New Roman',15, 'bold'), height=2, width=25, command=Optimize, bg="SteelBlue1")
-optimizeBtn.grid(row=12, rowspan=2, column=16, columnspan=5)
+optimizeBtn = Button(top, text='Optimize', font=('Times New Roman',15, 'bold'), height=2, width=25, command=Optimize, highlightbackground="#212121", fg='#FFFFFF')
+optimizeBtn.grid(row=12, rowspan=2, column=17, columnspan=5)
 
 # number of lineup setting
-setting1 = Label(top, text='Number of Lineups:', bg="snow4")
+setting1 = Label(top, text='Number of Lineups:', bg="#212121", fg='#FFFFFF')
 setting1.grid(row=2, sticky=W)
-settingLineupsNum = Label(top,textvariable=lineups, bg="snow4")
+settingLineupsNum = Label(top,textvariable=lineups, bg="#212121", fg='#FFFFFF')
 settingLineupsNum.grid(row=2,column = 1, sticky=W)
 
 # user input for lineup setting
@@ -208,9 +209,9 @@ lineupNumInput.grid(row=2, column=2, sticky=W)
 lineupNumInput.bind('<Return>',(lambda event: enterPressed(lineupNumInput,lineups)))
 
 # number of player setting
-setting2 = Label(top, text='Number of Players: ', bg="snow4")
+setting2 = Label(top, text='Number of Players: ', bg="#212121", fg='#FFFFFF')
 setting2.grid(row=3, sticky=W)
-displayPlayersNum = Label(top,textvariable=players, bg="snow4")
+displayPlayersNum = Label(top,textvariable=players, bg="#212121", fg='#FFFFFF')
 displayPlayersNum.grid(row=3,column = 1, sticky=W)
 
 # user input max player setting
@@ -219,9 +220,9 @@ playerNumInput.grid(row=3, column=2,sticky=W)
 playerNumInput.bind('<Return>',(lambda event: enterPressed(playerNumInput,players)))
 
 # max spending cost setting
-setting3 = Label(top, text='Max Cost: ', bg="snow4")
+setting3 = Label(top, text='Max Cost: ', bg="#212121", fg='#FFFFFF')
 setting3.grid(row=4, sticky=W)
-displayCostNum = Label(top,textvariable=maxCost, bg="snow4")
+displayCostNum = Label(top,textvariable=maxCost, bg="#212121", fg='#FFFFFF')
 displayCostNum.grid(row=4,column = 1, sticky=W)
 
 # user input for max spending cost setting
@@ -230,13 +231,13 @@ costNumInput.grid(row=4, column=2,sticky=W)
 costNumInput.bind('<Return>',(lambda event: enterPressed(costNumInput,maxCost)))
 
 # max for specified category setting
-setting4 = Label(top, text='Max for specified category: ', bg="snow4")
+setting4 = Label(top, text='Max for specified category: ', bg="#212121", fg='#FFFFFF')
 setting4.grid(row = 5, sticky = W)
-setting4MaxNum = Label(top, text = '>', bg="snow4")
+setting4MaxNum = Label(top, text = '>', bg="#212121", fg='#FFFFFF')
 setting4MaxNum.grid(row = 5, column = 3, sticky = W)
 
 # for test purposes can delete later
-displayPosAmount = Label(top, textvariable = numPos, bg="snow4")
+displayPosAmount = Label(top, textvariable = numPos, bg="#212121", fg='#FFFFFF')
 displayPosAmount.grid(row = 5, column = 1, sticky = W)
 
 # user input for max specified category
@@ -245,19 +246,20 @@ maxCat.grid(row = 5, column = 2, sticky = W)
 maxCat.bind('<Return>',(lambda event: enterPressed(maxCat, numPos)))
 
 #manually create a config.txt file
-createConfig = Button(top, text='Create Configuration File', command=makeConfigFile, bg="SteelBlue1")
-createConfig.grid(row=15, column=0, sticky=E)
+createConfig = Button(top, text='Create Configuration File', command=makeConfigFile)
+createConfig.config(highlightbackground="#212121", fg='white')
+createConfig.grid(row=15,columnspan=2, sticky=E)
 
 #Load Preset configuration from drop down menu
-presetLabel = Label(top, text='Presets: ')
-presetLabel.grid(row = 13, column = 12, sticky = W)
+presetLabel = Label(top, text='Presets: ',bg="#212121", fg='#FFFFFF')
+presetLabel.grid(row = 13,column = 2, sticky = W)
 presetDropMenu = CreateDropMenu(top, 'Select Status',settings.presetConfigList)
-presetDropMenu.grid(row = 13, column = 13, sticky = W)
+presetDropMenu.grid(row = 13, column = 4, sticky = W)
 for file in [txt for txt in os.listdir("presets")
 if txt.endswith(".txt")]:
     presetDropMenu.children['menu'].add_command(label=file,command=lambda heading=file: presetDropMenu.dropDownVar.set(heading))
-loadPreset = Button(top, text='Load Preset', command=loadPresetConfig)
-loadPreset.grid(row=13, column=14, sticky = W)
+loadPreset = Button(top, text='Load Preset', command=loadPresetConfig, highlightbackground="#212121", fg='#FFFFFF')
+loadPreset.grid(row=13, column=14, columnspan=3, sticky = W)
 
 #=============
 # Split Frame
